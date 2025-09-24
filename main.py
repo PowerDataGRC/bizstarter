c0963c90bc574a1baba14c42818d19cc9461da07
shabestari <shabestari@gmail.com>
2025-09-24 03:30:42 +0000

    First commit

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import os
 
-from flask import Flask, send_file
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route("/")
-def index():
-    return send_file('src/index.html')
+def intro():
+    return render_template('intro.html')
+
+@app.route("/product-detail")
+def product_detail():
+    return render_template('product-detail.html')
+
+@app.route("/financial-forecast")
+def financial_forecast():
+    return render_template('financial-forecast.html')
+
+@app.route("/loan-calculator")
+def loan_calculator():
+    return render_template('loan-calculator.html')
 
 def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
 
 if __name__ == "__main__":
     main()
