de1b543579ede6e9409b0252ee5338b59a552911
Murray <shabestari@gmail.com>
2025-09-29 21:23:56 -0500

    Update build.sh to python3

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 
 # Run the database initialization function from main.py
-python -c "from main import vercel_build; vercel_build()"
+python3 -c "from main import vercel_build; vercel_build()"
