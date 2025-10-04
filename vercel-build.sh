8451980d7423a06d14d179e23c8ffb4eeee002ab
max <shabestari@gmail.com>
2025-10-04 13:33:08 -0500

    FIX: more robust vercel build script

--- vercel-build.sh
+++ vercel-build.sh
@@ -3,5 +3,5 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 # Set the FLASK_APP environment variable so the flask command knows where to find the app
-export FLASK_APP=main.py
-flask db upgrade
+export FLASK_APP=main:app
+python -m flask db upgrade
