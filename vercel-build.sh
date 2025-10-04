8fbda42174105b0e8c77c0e1baf33c533bd6e743
max <shabestari@gmail.com>
2025-10-04 13:29:07 -0500

    FIX: set FLASK_APP in Vercel build script

--- vercel-build.sh
+++ vercel-build.sh
@@ -2,5 +2,6 @@
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
-
+# Set the FLASK_APP environment variable so the flask command knows where to find the app
+export FLASK_APP=main.py
 flask db upgrade
