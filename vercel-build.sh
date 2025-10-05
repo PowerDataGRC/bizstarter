066eae6ead857c22d20b4aee0647287fe13048ae
max <shabestari@gmail.com>
2025-10-04 23:38:23 -0500

    FIX: going round and round...2

--- vercel-build.sh
+++ vercel-build.sh
@@ -2,8 +2,11 @@
 # Exit on error and print commands for debugging
 set -ex
  
+echo "--- Installing dependencies ---"
+pip install -r requirements.txt
+
 echo "--- Running database migrations ---"
 export PYTHONPATH="."
 export FLASK_APP=main.py
-python3 -m flask db upgrade
+python -m flask db upgrade
 echo "--- Database migrations finished successfully ---"
