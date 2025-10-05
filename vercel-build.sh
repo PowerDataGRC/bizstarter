599c6d78af9f52286400777e8470c6e5a0a92b6a
max <shabestari@gmail.com>
2025-10-04 23:01:52 -0500

    FIX: another fix...2

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-set -e
+# Exit on error and print commands for debugging
+set -ex
 
+echo "--- Running database migrations ---"
 export PYTHONPATH="."
 export FLASK_APP=main.py
 python3 -m flask db upgrade
+echo "--- Database migrations finished successfully ---"
