8fdc920a85b3e91349093b1d697ae24a76129451
max <shabestari@gmail.com>
2025-10-04 23:55:31 -0500

    FIX: going round and round...6

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,14 @@
+import os
 from main import app
+from alembic.config import Config
+from alembic import command
+
+try:
+    with app.app_context():
+        migrations_dir = os.path.join(os.path.dirname(__file__), 'migrations')
+        alembic_cfg = Config(os.path.join(migrations_dir, "alembic.ini"))
+        alembic_cfg.set_main_option('script_location', migrations_dir)
+        alembic_cfg.set_main_option('sqlalchemy.url', app.config['SQLALCHEMY_DATABASE_URI'])
+        command.upgrade(alembic_cfg, "head")
+except Exception as e:
+    app.logger.error(f"Failed to apply database migrations: {e}")
