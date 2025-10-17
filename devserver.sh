98d3f61ca3d455493c7710e9e2115284ba124ff7
max <shabestari@gmail.com>
2025-10-16 21:05:02 -0500

    FIX: App logs user out at random

--- devserver.sh
+++ devserver.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . .venv/bin/activate
-export FLASK_SECRET_KEY='a_default_secret_key_for_development_only'
+export FLASK_SECRET_KEY='24c1bf8f45e5a508ff624ae87a159730ffdabf4e513e436f31b07e7edb959d79'
 python -u -m flask --app main run --host=0.0.0.0 -p ${PORT:-5000} --debug
